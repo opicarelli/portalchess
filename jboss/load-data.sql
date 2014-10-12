bf0016307a1ede119963fafe5943c1cb73d2d1dc
Marcos Oto Picarelli Prado <opicarelli@gmail.com>
2014-10-12 14:52:34 -0300

    #1: Maven plugins and dependecies

---
+++ jboss/load-data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user_t (active, birthdate, email, gender, password, profile, username) VALUES ('YES', '1987-08-14', 'opicarelli@gmail.com', 'MALE', '22e600615d4bd67e34108027eb6b452b', 'ADMIN', 'admin');
